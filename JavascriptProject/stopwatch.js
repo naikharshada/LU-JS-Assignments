e3714a06580c19050bcf5630b0b56914a653b466
Harshada Naik <68236767+naikharshada@users.noreply.github.com>
2020-07-31 22:52:07 +0530

    Add files via upload

---
+++ JavascriptProject/stopwatch.js
@@ -0,0 +1,31 @@
+
+ // Set the date we're counting down to
+  let d = prompt("ef","feb 8, 2021")
+  var countDownDate = new Date(`${d} 00:00:00`).getTime();
+  console.log();
+  
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("stopwatch").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("stopwatch").innerHTML = "EXPIRED";
+  }
+}, 1000);
